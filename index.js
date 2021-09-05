404734ef0ee4826859f3acdac3190ecdab34c81c
Matt <aboutmattlaw@gmail.com>
2021-09-05 15:54:22 -0400

    commit tree 12e96329f77b62d9b088fb9bb20828fdf099301e

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your code in this file!
+
+// Second Lab Attempt 
+
+
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// First Lab Attempt 
+
+// // Write your code in this file!
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const shortGreeting = `Welcome, ${currentUser.slice(0, 2)}!`;
